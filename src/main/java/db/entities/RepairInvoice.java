a17c04d1a15fc704a5626b9344da16973759c1b3
Sem Zhivotov <semzhivotov@gmail.com>
2022-09-01 23:34:11 +0200

    1

--- src/main/java/db/entities/RepairInvoice.java
+++
@@ -1,5 +0,0 @@
-package db.entities;
-
-public class RepairInvoice {
-
-}
--- src/main/java/util/PriceCalculator.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class PriceCalculator {
-
-    public static int getPrice (int carPrice, int driverPrice, int orderDays){
-        return (carPrice+driverPrice)*orderDays;
-    }
-
-}
